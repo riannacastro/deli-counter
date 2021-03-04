63763a84048ab91a7a600f79e8576b1fac187a0d
Rianna Castro <riannaxcastro@gmail.com>
2021-03-03 14:01:47 -1000

    commit tree 02912508228f8c014b1eab40d90a13bd12cdc8a0

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+require 'pry'
+
+def line(katz_deli)
+    if katz_deli == []
+    puts "The line is currently empty."
+
+    else 
+        string = "The line is currently:"
+        katz_deli.each_with_index do |name, index|
+            
+   string << " #{index + 1}. #{name}"
+end
+puts string
+ 
+end
+
+end
+
+def take_a_number(katz_deli, person)
+    katz_deli << person
+    puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
+end
+
+
+def now_serving(katz_deli)
+   if katz_deli == []
+    puts "There is nobody waiting to be served!"
+   else
+    puts "Currently serving #{katz_deli.shift}."
+   end
+end
+
+
+
+
